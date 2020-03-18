db273b144eed1165b370efdc5205aa4ede75c0c5
Mahaut <mmahaut@ensc.fr>
2020-03-18 12:11:47 +0100

    partial construction of shapes that allow automatic training of RL-networks

--- gym_CtF/gym_CtF/envs/CtF_env.py
+++ gym_CtF/gym_CtF/envs/CtF_env.py
@@ -9,6 +9,41 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class CtFEnv(gym.Env):
+      """
+    Description:
+    A random map is generated with obstacles. 
+    Two teams of agents are generated on opposite sides of the map.
+    They each must capture the other team's flag without being killed by their opponent.
+    First team to capture a flag wins.
+
+    Observation: 
+        Type: Box(4)
+        Num	Observation                 Min         Max
+        0	Cart Position             -4.8            4.8
+        1	Cart Velocity             -Inf            Inf
+        2	Pole Angle                 -24 deg        24 deg
+        3	Pole Velocity At Tip      -Inf            Inf
+        
+    Actions:
+        Type: Discrete(2)
+        Num	Action
+        0	Push cart to the left
+        1	Push cart to the right
+        
+    Reward:
+        Reward is 1 for every step taken, including the termination step
+    Starting State:
+        All observations are assigned a uniform random value in [-0.05..0.05]
+    Episode Termination:
+        Pole Angle is more than 12 degrees
+        Cart Position is more than 2.4 (center of the cart reaches the edge of the display)
+        Episode length is greater than 200
+        Solved Requirements
+        Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
+    """
+  
+  
+  
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
@@ -16,8 +51,9 @@ class CtFEnv(gym.Env):
     self.map = self.generateMap(100,40)
     # There shall be two teams to begin with
     self.nbTeamMembers = 5
-    # This is what the agents will be allowed to see each turn
-    self.state = np.zeros((self.nbTeamMembers*2,4))
+    # This is what the agents will be allowed to see each turn --> The observation space
+    # self.state = spaces.Box(low=0, high=2.0, shape=(self.nbTeamMembers * 2, 4), dtype=np.int32)
+    self.action_space = spaces.Box(low=0, high=2.0, shape=(self.nbTeamMembers * 2, 4), dtype=np.int32)
     self.rewards = np.zeros(self.nbTeamMembers*2)
 
     # There might come a time where teams are generated in a locked space
@@ -47,9 +83,12 @@ class CtFEnv(gym.Env):
       self.map[len(self.map)-3][len(self.map[0])-(i+2)*2] = True
 
   def step(self, action) :
-    # Maybe later add some more checkups for data structure
+    # checkups for data structure
     if len(self.agents) != len(action):
       print("Invalid action : number of agents has been set to " + len(self.agents) + " but you only gave an action of size " + len(action))
+
+    assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+
       return [self.state, self.rewards, self.done, self.add]
     else:
       self.state = []
