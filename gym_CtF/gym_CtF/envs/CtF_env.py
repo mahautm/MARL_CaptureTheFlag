abab6056abcf98abb5cd3859386274bbfcc3633b
Mahaut <mmahaut@ensc.fr>
2020-04-03 10:52:35 +0200

    type

--- gym_CtF/gym_CtF/envs/CtF_env.py
+++ gym_CtF/gym_CtF/envs/CtF_env.py
@@ -50,9 +50,9 @@ class CtFEnv(gym.Env):
     # This is what the agents will be allowed to see each turn --> The observation space
     #Documentation hardly exists, 
     # from reading the code I'll just align binary, event if that seems like a sub-optimal solution
-    self.observation_space = spaces.Box(low=0, high=1, shape=(self.observation_size, self.observation_size,self.nbTeamMembers,4), dtype=np.uint8)
+    self.observation_space = spaces.Box(low=0, high=2, shape=(self.observation_size, self.observation_size,self.nbTeamMembers,4), dtype=np.int64)
 
-    self.action_space = spaces.MultiBinary(2 * self.nbTeamMembers*5)
+    self.action_space = spaces.Box(low=0, high=2, shape=(2 * self.nbTeamMembers*5),dtype=np.int64)
     self.rewards = np.zeros(self.nbTeamMembers*2)
     self.np_random = None 
     self.seed()
